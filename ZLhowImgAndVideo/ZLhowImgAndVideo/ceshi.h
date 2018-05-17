024ae0b8e65c5d6d310fc183d37363377123c5fa
lizhenwen <1013372794@qq.com>
2018-05-17 15:18:42 +0800

    测试新增文件

---
+++ ZLhowImgAndVideo/ZLhowImgAndVideo/ceshi.h
@@ -0,0 +1,13 @@
+//
+//  ceshi.h
+//  ZLhowImgAndVideo
+//
+//  Created by ZhenwenLi on 2018/5/17.
+//  Copyright © 2018年 lizhenwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ceshi : UIView
+
+@end
---
+++ ZLhowImgAndVideo/ZLhowImgAndVideo/ceshi.m
@@ -0,0 +1,21 @@
+//
+//  ceshi.m
+//  ZLhowImgAndVideo
+//
+//  Created by ZhenwenLi on 2018/5/17.
+//  Copyright © 2018年 lizhenwen. All rights reserved.
+//
+
+#import "ceshi.h"
+
+@implementation ceshi
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
